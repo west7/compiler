65fc3a90509f38483e7cb003b18dcebd2ad6a042
Guilherme Westphall <guilhermewestphall@gmail.com>
2025-07-31 08:30:05 -0300

    Add operator and brackets precedence

--- src/main.rs
+++ src/main.rs
@@ -2,63 +2,32 @@
 
 mod parser;
 
-use parser::EletronParser;
-use parser::Rule;
+use parser::*;
 use pest::Parser;
 use pest::iterators::{Pair, Pairs};
 use std::fs;
 
 fn main() {
-    let file_e = fs::read_to_string("src/main.e").expect("File Not Found");
+    let src = fs::read_to_string("src/main.e").expect("File Not Found");
 
-    match EletronParser::parse(Rule::program, &file_e) {
-        Ok(pairs) => {
-            //println!("{:#?}", pairs);
-            for pair in pairs.into_iter() {
-                for expr in pair.into_inner() {
-                    // println!("PAR {:#?}", pair);
-                    match expr.as_rule() {
-                        Rule::expr => {
-                            let result = evaluate_expr(expr);
-                            println!("= {}", result)
-                        }
-                        _ => println!("Unhandled: {:#?}", expr),
-                    }
+    match EletronParser::parse(Rule::program, &src) {
+        Ok(mut pairs) => {
+            let inner_pairs = pairs.next().unwrap().into_inner();
+            
+            for pair in inner_pairs {
+                match &pair.as_rule() {
+                    Rule::sum_expr => {
+                        print!("{} ", pair.as_str());
+                        let result = eval_sum_expr(pair);
+                        println!("= {}", result);
+                    }   
+                    Rule::EOI => {}
+                    _ => eprintln!("Unhandled: {:#?}", pair)                 
                 }
             }
         }
         Err(e) => {
             eprintln!("Parsing Failed: {}\n", e);
         }
-    }
-}
-
-fn read_pair(pair: Pair<Rule>) {
-    todo!()
-}
-
-fn evaluate_expr(pair: Pair<Rule>) -> f64 {
-    let mut inner = pair.into_inner();
-    let mut acc = evaluate_term(inner.next().unwrap());
-
-    while let Some(op) = inner.next() {
-        let next = evaluate_term(inner.next().unwrap());
-
-        match op.as_str() {
-            "+" => acc += next,
-            "-" => acc -= next,
-            "*" => acc *= next,
-            "/" => acc /= next,
-            _ => unreachable!(),
-        }
-    }
-
-    acc
-}
-
-fn evaluate_term(pair: Pair<Rule>) -> f64 {
-    match pair.as_rule() {
-        Rule::number => pair.as_str().trim().parse().unwrap(),
-        _ => unreachable!(),
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,57 @@
+use pest::iterators::Pair;
 use pest_derive::Parser;
 
 #[derive(Parser)]
 #[grammar = "eletron.pest"]
 pub struct EletronParser;
+
+
+pub fn eval_expr(pair: Pair<Rule>, nl_fn: impl Fn(Pair<Rule>) -> f64) -> f64 {
+    let mut inner = pair.into_inner();
+    let mut acc = nl_fn(inner.next().unwrap());
+
+    while let Some(op) = inner.next() {
+        let rhs = nl_fn(inner.next().unwrap());
+        match op.as_str() {
+            "+" => acc += rhs,
+            "-" => acc -= rhs,
+            "*" => acc *= rhs,
+            "/" => acc /= rhs,
+            "**" => acc = acc.powf(rhs),
+            _ => unreachable!(),
+        }
+    }
+    acc
+}
+
+pub fn eval_sum_expr(pair: Pair<Rule>) -> f64 {
+    eval_expr(pair, eval_mul_expr)
+}
+
+pub fn eval_mul_expr(pair: Pair<Rule>) -> f64 {
+    eval_expr(pair, eval_pow_expr)
+}
+
+pub fn eval_pow_expr(pair: Pair<Rule>) -> f64 {
+    let mut inner = pair.into_inner();
+    let lhs = eval_factor_expr(inner.next().unwrap());
+
+    if let Some(op) = inner.next() {
+        let rhs = eval_pow_expr(inner.next().unwrap()); // Chamada recursiva aqui
+        match op.as_str() {
+            "**" | "^" => lhs.powf(rhs),
+            _ => unreachable!(),
+        }
+    } else {
+        lhs
+    }
+}   
+
+pub fn eval_factor_expr(pair: Pair<Rule>) -> f64 {
+    let inner = pair.into_inner().next().unwrap();
+    match inner.as_rule() {
+        Rule::number => inner.as_str().trim().parse().unwrap(),
+        Rule::sum_expr => eval_sum_expr(inner),
+        _ => unreachable!(),
+    }
+}
